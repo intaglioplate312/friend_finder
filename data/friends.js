acc2d3400d0405237f47ce14acc436c7a876f32c
intaglioplate312 <helenelizabeth@me.com>
2017-08-08 15:01:01 -0500

    almost ready for required commit

--- data/friends.js
+++ data/friends.js
@@ -1,13 +1,71 @@
-// 6. Determine the user's most compatible friend using the following as a guide:
+//data as an array of objects.
+//json
+var fiends = [{
+        "name": "Riz Ahmed",
+        "photo": "../images/Riz_Ahmed.png",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Nigel Molesworth",
+        "photo": "../images/Ronald-Searle-Molesworth--007.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Anna Howe",
+        "photo": "../images/anna_howe.jpg",
+        "scores": [
+            5,
+            4,
+            3,
+            2,
+            1,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "Elinor Dashwood",
+        "photo": "../images/e_dashwood.jpg",
+        "scores": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+    },
 
-//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-//    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
-//      * Example: 
-//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-//        * Total Difference: **2 + 1 + 2 =** **_5_**
-//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on. 
-//    * The closest match will be the user with the least amount of difference.
+];
 
-// 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-//    * The modal should display both the name and picture of the closest match.o
+//create export module for use in apiRoutes var friends = require('../friend.js')
+module.exports = friends;
