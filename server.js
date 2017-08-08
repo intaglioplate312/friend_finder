79518a1c19c3ed2b67b4bef608a87996d738fd06
Your Actual Name <Your Actual Email>
2017-08-08 15:48:23 -0500

    stash

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-// Your `server.js` file should require the basic npm packages we've used in class:
-`express`,
-`body-parser`
-and
-    `path`.
+// // Your `server.js` file should require the basic npm packages we've used in class:
+// `express`,
+// `body-parser`
+// and
+//     `path`.
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
 var path = require('path')
-var bodyParser = require('body-parser')
-var express = require('express')
-var app = express()
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 
 app.get('/', function(req, res) {
     res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
